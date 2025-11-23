70ba4add66cca44148314673ee799db635282def
Namit Magotra <namit130506@gmail.com>
2025-11-23 15:49:38 +0530

    added test case

---
+++ test/TestMathsModule.py
@@ -0,0 +1,11 @@
+#Test Maths Question Bank
+
+#Test module with only 1 question per type
+
+maths_mcq=[{"question": "Derivative of x² is:", "options": ["x", "2x", "x²"], "answer": "2x"},]
+
+maths_fill=[{"question": "The value of π (approx) is _____.", "answer": "3.14"},]
+
+maths_short=[{"question": "Define a rational number."},]
+
+maths_long=[{"question": "Explain the steps to solve a quadratic equation by factorization."},]
---
+++ test/test.py
@@ -0,0 +1,25 @@
+#Test Case for maths pyq
+
+import random
+import TestMathsModule
+
+def main():
+    random.seed(0)
+    q= TestMathsModule.maths_mcq[0]
+
+    print("Q:", q["question"])
+    print("Options:", q["options"])
+    answer_text = q["answer"]
+    options = q["options"]
+    answer_index = 1
+
+    for i in range(len(options)):
+        if options[i].lower() == answer_text.lower():
+            answer_index = i + 1
+            break
+
+    print("Correct option number:", answer_index)
+    print("-----Here you go! ^-^-----")
+
+if __name__ == "__main__":
+    main()
