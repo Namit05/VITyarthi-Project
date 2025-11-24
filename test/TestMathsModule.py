31280817d392de6a2c38aafd01f72f19c02b816f
Namit Magotra <namit130506@gmail.com>
2025-11-24 09:17:23 +0530

    added a new test case

--- test/TestMathsModule.py
+++ test/TestMathsModule.py
@@ -1,11 +1,12 @@
 #Test Maths Question Bank
 
 #Test module with only 1 question per type
-
-maths_mcq=[{"question": "Derivative of x² is:", "options": ["x", "2x", "x²"], "answer": "2x"},]
+#1 maths mcq 1 fill ups 1 short answer and 1 long answer as example
+maths_mcq=[{"question": "Derivative of x^2 is:", "options": ["x", "2x", "x^2"], "answer": "2x"},]
 
 maths_fill=[{"question": "The value of π (approx) is _____.", "answer": "3.14"},]
 
 maths_short=[{"question": "Define a rational number."},]
 
 maths_long=[{"question": "Explain the steps to solve a quadratic equation by factorization."},]
+#this keeps the test case simple as these are just examples and keeps it fun
--- test/test.py
+++ test/test.py
@@ -1,27 +1,27 @@
 #Test Case for maths pyq
 
-#Only for maths
+#Only for maths just as a test case
 import random
 import TestMathsModule
 
 #Main menu automatically gives u mcq options and answer for the test case!
 def main():
     random.seed(0)
-    q= TestMathsModule.maths_mcq[0]
-
-    print("Q:", q["question"])
-    print("Options:", q["options"])
-    answer_text = q["answer"]
-    options = q["options"]
-    answer_index = 1
+    q=TestMathsModule.maths_mcq[0]
+#will print all 3 menus without input from user and also give correct answer!
+    print("Q:",q["question"])
+    print("Options:",q["options"])
+    answer_text=q["answer"]
+    options=q["options"]
+    answer_index=1
 
     for i in range(len(options)):
-        if options[i].lower() == answer_text.lower():
-            answer_index = i + 1
+        if options[i].lower()==answer_text.lower():
+            answer_index=i+1
             break
+#Kept it simple for test case just to show all the three types of selections
+    print("Correct option number:",answer_index)
+    print("-----Here you go!^-^-----")
 
-    print("Correct option number:", answer_index)
-    print("-----Here you go! ^-^-----")
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
