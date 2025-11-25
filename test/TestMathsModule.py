dc111ad810b735476e8d03ca89fbc48c3155e4dd
Namit Magotra <namit130506@gmail.com>
2025-11-25 23:52:16 +0530

    Update test module name and math question wording

--- test/TestMathsModule.py
+++ test/TestMathsModule.py
@@ -1,8 +1,8 @@
-#Test Maths Question Bank
+#TestModule Of MathsQuestions
 
 #Test module with only 1 question per type
 #1 maths mcq 1 fill ups 1 short answer and 1 long answer as example
-maths_mcq=[{"question": "Derivative of x^2 is:", "options": ["x", "2x", "x^2"], "answer": "2x"},]
+maths_mcq=[{"question": "The Derivative of x^2 is:", "options": ["x", "2x", "x^2"], "answer": "2x"},]
 
 maths_fill=[{"question": "The value of π (approx) is _____.", "answer": "3.14"},]
 
